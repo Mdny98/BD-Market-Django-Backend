d0aec0dc014bb61151ff68f4675199583f03f5ae
UncleKasra <48312344+UncleKasra@users.noreply.github.com>
2020-08-04 10:53:32 +0430

    correct indentation

--- customer/models.py
+++ customer/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Customer(models.Model):
-     """
-        Represent Customers Informations 
-     """
-    firstname= models.CharField(max_length=250)
-    lastname= models.CharField(max_length=250)
-    city= models.CharField(max_length=250)
-    postalcode= models.IntegerField(max_length=250)
-    phone= models.IntegerField(max_length=250)
-    email= models.CharField(max_length=250)
-    username= models.CharField(max_length=250)
-    password= models.IntegerField(max_length=250)
-    dateentered= models.DateTimeField(max_length=250)
+   """
+      Represent Customers Informations 
+   """
+   firstname= models.CharField(max_length=250)
+   lastname= models.CharField(max_length=250)
+   city= models.CharField(max_length=250)
+   postalcode= models.IntegerField(max_length=250)
+   phone= models.IntegerField(max_length=250)
+   email= models.CharField(max_length=250)
+   username= models.CharField(max_length=250)
+   password= models.IntegerField(max_length=250)
+   dateentered= models.DateTimeField(max_length=250)

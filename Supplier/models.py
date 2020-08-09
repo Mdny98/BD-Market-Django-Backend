c7422634fcbf5a62415900bf0fe505d2c28649e5
armin <armin@armin.com>
2020-08-09 15:00:27 +0430

    complite views and urls

--- Supplier/models.py
+++ Supplier/models.py
@@ -4,18 +4,19 @@ from content.models import Product
 
 # Create your models here.
 
+
 class Supplier_Profile(models.Model):
     """
         a table for all supplier
-    """ 
+    """
     city_choices = [
         ('tehran', 'Tehran'),
-        ('ardebil','Ardebil'),
-        ('rasht','Rasht'),
-        ('kerman','Kerman'),
-        ('ahvaz','Ahvaz')
-    ] # to baghie sharha namayandeghi nadarim 
-    supplier = models.OneToOneField(User, on_delete= models.CASCADE)
+        ('ardebil', 'Ardebil'),
+        ('rasht', 'Rasht'),
+        ('kerman', 'Kerman'),
+        ('ahvaz', 'Ahvaz')
+    ]  # to baghie sharha namayandeghi nadarim
+    supplier = models.OneToOneField(User, on_delete=models.CASCADE)
     company_name = models.CharField(max_length=50)
     address = models.TextField(max_length=500)
     city = models.CharField(max_length=50, choices=city_choices)
@@ -23,13 +24,11 @@ class Supplier_Profile(models.Model):
     def __str__(self):
         return self.company_name
 
+
 class Product_Supplier(models.Model):
     """
     a table between products and supplier
-    """    
+    """
     supplier_id = models.ForeignKey(Supplier_Profile, on_delete=models.CASCADE)
-    product_id = models.ForeignKey(Product, on_delete= models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     stock = models.IntegerField(default=0)
-
-    def __str__(self):
-        return self.supplier_id

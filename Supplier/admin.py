34e96b3ff58723097ac4d0cc941759eea1796a60
armin <armin@armin.com>
2020-08-09 12:36:46 +0430

    complite views and urls

--- Supplier/admin.py
+++ Supplier/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from Supplier.models import  Supplier_Profile , Product_Supplier
 # Register your models here.
+admin.site.register(Supplier_Profile)
+admin.site.register(Product_Supplier)
--- Supplier/views.py
+++ Supplier/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
-
+from django.shortcuts import render 
+from .models import *
 # Create your views here.
+def show_all_suppliers(request):
+    return render(request, 'Supplier/supplier.html')
+
+def show_supplier(request):
+    pass

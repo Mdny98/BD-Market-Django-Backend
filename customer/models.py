8b3aa6c57e5b352070e9f724edeb0e158546db28
Mohammad <mohammaddonyavi1999@gmail.com>
2020-08-09 15:43:17 +0430

    customer model

--- customer/models.py
+++ customer/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class CustomerProfile(models.Model):
    """
@@ -12,11 +12,11 @@ class CustomerProfile(models.Model):
       ]
    phone= models.IntegerField(blank=True, null=True)
 
-class CustomerAddress():
+class CustomerAddress(models.Model):
    """
       Represent adresses of customers
    """
    address=city= models.CharField(max_length=250)
-   customer_id=models.ForeignKey(CustomerProfile, on_delete=models.SET_NULL)
+   customer_id=models.ForeignKey(CustomerProfile, on_delete=models.CASCADE)
    city= models.CharField(max_length=250)
    postalcode= models.IntegerField(blank=True, null=True)

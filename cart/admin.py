4f5a33c9fce2c980a33df0588eb7ea3b30e328fa
armin <armin@armin.com>
2020-08-09 17:43:47 +0430

    complite cart

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import  Delivery , Cart , OrderItem
 
-# Register your models here.
+admin.site.register(Delivery)
+admin.site.register(Cart)
+admin.site.register(OrderItem)

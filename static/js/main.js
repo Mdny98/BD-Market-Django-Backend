d38d78ee6037cb98acba23386e5c69214b43e9cb
UncleKasra <48312344+UncleKasra@users.noreply.github.com>
2020-08-13 12:05:52 +0430

    slider completed

--- static/js/main.js
+++ static/js/main.js
@@ -21,20 +21,3 @@ function openCity(evt, cityName) {
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
 
-//   $('.owl-carousel').owlCarousel({
-//     rtl:true,
-//     loop:true,
-//     margin:10,
-//     nav:true,
-//     responsive:{
-//         0:{
-//             items:1
-//         },
-//         600:{
-//             items:3
-//         },
-//         1000:{
-//             items:5
-//         }
-//     }
-// })

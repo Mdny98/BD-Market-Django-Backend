1597600c3b148da91ce0017113ff03dd484edcb2
UncleKasra <48312344+UncleKasra@users.noreply.github.com>
2020-08-19 18:51:49 +0430

    complete sidebar filtering

--- content/urls.py
+++ content/urls.py
@@ -7,6 +7,7 @@ app_name = 'content'
 urlpatterns = [
     path('', views.home, name='home'),
     path('categories/', views.categories, name='categories'),
-    path('products/<int:cat_pk>', views.products, name='products'),
+    # path('products/<int:cat_pk>', views.products, name='products'),
+    path('products/(?P<cat_pk>[0-9]+)$', views.products, name='products'),
     path('productdetails/<int:product_pk>', views.productdetails, name='productdetails'),
 ]
--- content/views.py
+++ content/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import SubCategory, Product
+from .models import SubCategory, Product, Attribute
 from Supplier.models import ProductSupplier
 # Create your views here.
 
@@ -27,27 +27,50 @@ def categories(request):
         main_cats = all_cats.filter(parent_category = None)
         return render(request, 'content/categories.html', {'all_cats':all_cats, 'main_cats':main_cats})
 
+def get_parent_cats(cat):
+    catlst = []
+    f = 1
+    catlst.append(cat)
+    while f:
+        catlst.append(cat.parent_category)
+        cat = cat.parent_category
+        if cat.parent_category == None:
+            f = 0
+    catlst.reverse()
+    return catlst
+    
 
 def products(request, cat_pk):
     if request.method == 'GET':
         all_products = Product.objects.filter(subcategory_id=cat_pk)
+        q = all_products
+        print(request.GET)
+        for k, v in request.GET.items():
+            tmpq = Attribute.objects.get(attr_title=k)
+            qtype = tmpq.pro_attr.all()[0].value_type
+            print(f'\nqtype is {qtype}')
+            if qtype == 'BOOL':
+                q = q.filter(pro_attr__attr_id__attr_title=k, pro_attr__bool_value=v)
+            elif qtype == 'INT':
+                q = q.filter(pro_attr__attr_id__attr_title=k, pro_attr__int_value=v)
+            elif qtype == 'TXT':
+                q = q.filter(pro_attr__attr_id__attr_title=k, pro_attr__text_value=v)
+
+            # print(f'\nk is {k}')
+            # print(f'\nk type is {type(k)}')
+            # print(f'\nq is {q}')
+
         cat = SubCategory.objects.get(pk=cat_pk)
-        return render(request, 'content/products.html', {'all_products':all_products, 'cat':cat})
+        catlst = get_parent_cats(cat)
+        return render(request, 'content/products.html', 
+            {'all_products':q, 'catlst':catlst, 'cat':cat})
 
 
 def productdetails(request, product_pk):
     if request.method == 'GET':
         this_product = Product.objects.get(pk=product_pk)
         this_product_suppliers = ProductSupplier.objects.filter(product_id=this_product)
-        catlst = []
-        f = 1
-        tmp = this_product.subcategory_id
-        catlst.append(tmp)
-        while f:
-            catlst.append(tmp.parent_category)
-            tmp = tmp.parent_category
-            if tmp.parent_category == None:
-                f = 0
-        catlst.reverse()
+        cat = this_product.subcategory_id
+        catlst = get_parent_cats(cat)
         return render(request, 'content/productdetails.html', 
         {'this_product':this_product, 'catlst':catlst, 'this_product_suppliers':this_product_suppliers})

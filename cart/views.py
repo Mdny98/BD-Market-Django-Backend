e4c9161f27d60fe6f222368c77d20f634fefc0f8
UncleKasra <48312344+UncleKasra@users.noreply.github.com>
2020-08-20 17:27:56 +0430

    add product to cart

--- cart/views.py
+++ cart/views.py
@@ -4,8 +4,19 @@ from django.http import HttpResponse
 
 from .models import Cart
 
+
 def showcart(request):
-    current_customer = request.user.customer
-    current_cart = Cart.objects.get(customer_id=current_customer, status="u")
-    
-    return render(request, 'cart/showcart.html', {'current_cart':current_cart})
+    if request.method == 'GET':
+        current_customer = request.user.customer
+        current_cart = Cart.objects.get(customer_id=current_customer, status="u")
+        
+        return render(request, 'cart/showcart.html', {'current_cart':current_cart})
+
+    if request.method == 'POST':
+        current_customer = request.user.customer
+        prosup = request.POST.get('prosup')
+        current_cart, created = Cart.objects.get_or_create(
+                                    customer_id=current_customer,
+                                    status='u',)
+        OrderItem(cart_id=current_cart, product_supplier_id=prosup, number=1)
+        return render(request, 'cart/showcart.html')

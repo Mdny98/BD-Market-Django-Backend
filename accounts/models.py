21ba40715949adb8e7f89024dddbc29c1b5160a6
armin <armin@armin.com>
2020-08-23 13:20:53 +0430

    update database

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 
 class User(AbstractUser):
     is_customer = models.BooleanField(default=False)
     is_supplier = models.BooleanField(default=False)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    
     
 
 gender_choices = [
@@ -22,14 +24,14 @@ city_choices = [
 
     
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, primary_key = True)
     phone_number = models.CharField(max_length=20)
     gender = models.CharField(max_length=50, choices=gender_choices)
     city = models.CharField(max_length=50, choices=city_choices)
 
 
 class Supplier(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE, primary_key = True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete = models.CASCADE, primary_key = True)
     phone_number = models.CharField(max_length=20)
     company_name = models.CharField(max_length=50)
     address = models.TextField(max_length=500)

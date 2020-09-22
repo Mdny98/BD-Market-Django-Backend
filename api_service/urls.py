3dfe104a26d8ddc64946e38fb2a7698b4dbdefd4
UncleKasra <48312344+UncleKasra@users.noreply.github.com>
2020-09-22 11:05:16 +0330

    start config JWT

--- api_service/urls.py
+++ api_service/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
 
 from . import views
 
@@ -21,5 +22,7 @@ router.register('orderitem', views.OrderItemView, basename='order')
 
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
